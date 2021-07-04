eb97d1bfb940d8b62c599c7f5f440f4d1d43258d
jbped <pedigojacob@gmail.com>
2021-07-04 17:58:39 -0600

    linked mysql to server.js

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const mysql = require("mysql2")
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to db
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user:"root",
+        password:"JaPe001!",
+        database: "election"
+    },
+    console.log("Success -- Connected to election database")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 app.use((req,res) => {
     res.status(404).end();
