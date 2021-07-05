b819cf9ddc86d18d5a8d49453da1822a840f8421
jbped <pedigojacob@gmail.com>
2021-07-04 18:08:53 -0600

    initial sql queries

--- server.js
+++ server.js
@@ -12,21 +12,47 @@ app.use(express.json());
 const db = mysql.createConnection(
     {
         host: "localhost",
-        user:"root",
-        password:"JaPe001!",
+        user: "root",
+        password: "JaPe001!",
         database: "election"
     },
     console.log("Success -- Connected to election database")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-})
 
-app.use((req,res) => {
+// Create candidate 
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(result)
+// })
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// db.query(`DELETE FROM candidates WHERE id=?`, 1, (err, result)  => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+app.use((req, res) => {
     res.status(404).end();
-})
+});
 
 app.listen(PORT, () => {
-    console.log (`The server is running on port ${PORT}`);
-})
+    console.log(`The server is running on port ${PORT}`);
+});
